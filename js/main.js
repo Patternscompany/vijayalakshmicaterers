632513fbbda68a33e63b133ea275d59612446b79
nandu1-k <121676407+nandu1-k@users.noreply.github.com>
2024-12-27 13:03:37 +0530

    n

--- js/main.js
+++ js/main.js
@@ -82,6 +82,33 @@
             }
         }
     });
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+			0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:1
+            }
+        }
+    });
 
 
     // Testimonial carousel
